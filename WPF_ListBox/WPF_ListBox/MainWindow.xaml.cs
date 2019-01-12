5e2530145c738615608eed801ee8588c9807cf53
Pierre-Luc Bruyere <proteama@yahoo.com>
2019-01-12 15:02:58 -0500

    WPF ListBox

---
+++ WPF_ListBox/WPF_ListBox/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_ListBox
+{
+  public class Match
+  {
+    public int Score1 { get; set; }
+    public int Score2 { get; set; }
+    public string Team1 { get; set; }
+    public string Team2 { get; set; }
+    public int Completion { get; set; }
+  }
+
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      List<Match> matches = new List<Match>();
+      matches.Add(new Match() { Team1 = "Bayern Munich", Team2 = "Real Madrid", Score1 = 3, Score2 = 2, Completion = 85 });
+      matches.Add(new Match() { Team1 = "PSG", Team2 = "FC Barcelona", Score1 = 5, Score2 = 3, Completion = 5 });
+      matches.Add(new Match() { Team1 = "Athelico Madrid", Team2 = "Arsenal", Score1 = 1, Score2 = 2, Completion = 50 });
+
+      lbMatches.ItemsSource = matches;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      if (lbMatches.SelectedItem != null)
+      {
+        Match match = lbMatches.SelectedItem as Match;
+        MessageBox.Show("Selected match: " +
+                        match.Team1 + " (" +
+                        match.Score1 + " - " +
+                        match.Score2 + ") " +
+                        match.Team2 + " ");
+      }
+    }
+  }
+}
